aaa9ef2b1dee1415d1553890e99c329ca2f24287
Wayne Sun <wtse.sun@gmail.com>
2019-04-22 14:53:38 -0700

    Implement a node to id:app using ReactDOM.render()

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// TODO
+var GroceryList = (props) => (
+  <div>
+    <p>Grocery is open now!</p>
+    
+  </div>
+);
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
+
+// var GroceryList = (props) => (
+//   <ul>
+//     <li>{props.groceryItems[0]}</li>
+//     <li>{props.groceryItems[1]}</li>
+//   </ul>
+// );
